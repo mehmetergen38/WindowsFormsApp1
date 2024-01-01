3e3cdaed34060478a1bc6a194db36f8a6f9fe557
ergen <ergen@MEHMETERGEN>
2024-01-02 02:26:38 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/BAKIMcs.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class BAKIM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(383, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PLAKA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(361, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BAKIM BİLGİSİ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(370, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "BAKIM TARİHİ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(343, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(343, 320);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(138, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(364, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "KAYDET";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(154, 180);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(490, 96);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // BAKIM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "BAKIM";
+            this.Text = "BAKIM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string constring = ("Data Source=localhost\\SQLEXPRESS;Initial Catalog=bakım;Integrated Security=True;Encrypt=False");
+        SqlConnection baglan = new SqlConnection(constring);
+
+        public void kayitlari_getir()
+        {
+            baglan.Open();
+            string getir = "select * from bakım";
+            SqlCommand komut = new SqlCommand(getir, baglan);
+
+            SqlDataAdapter ad = new SqlDataAdapter(komut);
+
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglan.Close();
+
+
+
+
+
+        }
+
+
+        public void verisil(int mno)
+        {
+            string sil = "Delete from bakım Where mno=@mno";
+
+            SqlCommand komut = new SqlCommand(sil, baglan);
+            baglan.Open();
+            komut.Parameters.AddWithValue("@mno", mno);
+
+            komut.ExecuteNonQuery();
+            baglan.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'bakımDataSet2.bakım' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+
+            // TODO: Bu kod satırı 'bakımDataSet1.bakım' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.bakımTableAdapter1.Fill(this.bakımDataSet1.bakım);
+            // TODO: Bu kod satırı 'bakımDataSet.bakım' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.bakımTableAdapter.Fill(this.bakımDataSet.bakım);
+
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            object kayıt = "Select * from bakım Where plaka=@plaka";
+            SqlCommand komut = new SqlCommand((string)kayıt,baglan);
+
+            komut.Parameters.AddWithValue("@plaka", textBox1.Text);
+
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            baglan.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow drow in dataGridView1.SelectedRows)
+            {
+                int mno = Convert.ToInt32(drow.Cells[0].Value);
+                verisil(mno);
+                kayitlari_getir();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (baglan.State == ConnectionState.Closed)
+                {
+                    baglan.Open();
+                    string kayıt = "insert into bakım (plaka,marka,model,ureti_yili,renk,kilometre,bakim_km,son_bakim_bilgi,son_bakim_tarih) values (@plaka," +
+                        "@marka,@model,@ureti_yili,@renk,@kilometre,@bakim_km,@son_bakim_bilgi,@son_bakim_tarih)";
+                    SqlCommand komut = new SqlCommand(kayıt, baglan);
+                    komut.Parameters.AddWithValue("@plaka", textBox2.Text);
+                    komut.Parameters.AddWithValue("@marka", textBox3.Text);
+                    komut.Parameters.AddWithValue("@model", textBox4.Text);
+                    komut.Parameters.AddWithValue("@ureti_yili", textBox7.Text);
+                    komut.Parameters.AddWithValue("@renk", textBox9.Text);
+                    komut.Parameters.AddWithValue("@kilometre", textBox6.Text);
+                    komut.Parameters.AddWithValue("@bakim_km", textBox8.Text);
+                    komut.Parameters.AddWithValue("@son_bakim_bilgi", textBox5.Text);
+                    komut.Parameters.AddWithValue("@son_bakim_tarih", textBox10.Text);
+
+                    komut.ExecuteNonQuery();
+
+                    MessageBox.Show("KAYIT EKLEME İŞLEMİ TAMAMLANDI");
+                }
+
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show("BİR HATA VAR" + hata.Message);
+            }
+        }
+    }
+}
